dea07cdc6462739506eb4cef72b9fde54450f47d
Ian Taylor <ian@iantaylor.xyz>
2021-02-11 11:31:22 -0700

    Fixed NaN handling in logsumexp and logdiffexp helpers

--- R/helpers.R
+++ R/helpers.R
@@ -8,29 +8,47 @@
 # Helper function to compute log(exp(a) - exp(b))
 # a and b should be of equal length, but no checking is done
 logdiffexp <- function(a, b) {
-  if (any(a < b)) {
+
+  diffs <- b - a
+
+  # Define cases
+  eithernan <- is.nan(a) | is.nan(b)
+  bothinf <- is.infinite(a) & is.infinite(b) & (a == b)
+  blarger <- (!eithernan) & (a < b)
+  smalldiff <- (!eithernan) & (!bothinf) & (!blarger) & (abs(diffs) < log(2))
+  largediff <- (!eithernan) & (!bothinf) & (!blarger) & (abs(diffs) >= log(2))
+
+  if (any(blarger)) {
     warning("a must be larger than b. NaNs produced")
   }
+
   # Split computation according to
   # https://cran.r-project.org/web/packages/Rmpfr/vignettes/log1mexp-note.pdf
-  diffs <- b - a
-  diffs[(b == -Inf) & (a == -Inf)] <- 0 # Fix case when a == b == -Inf
-  smalldiff <- (abs(diffs) < log(2))
   val <- rep(0, length(diffs))
   # For large differences, use log1p(-exp())
-  val[!smalldiff] <- log1p(-exp(diffs[!smalldiff]))
+  val[largediff] <- log1p(-exp(diffs[largediff]))
   # For small differences, use log(-expm1())
   val[smalldiff] <- log(-expm1(diffs[smalldiff]))
+  # For either value being nan, the result is nan
+  val[eithernan] <- NaN
+  # If both are infinite and equal, the result is the same value as both
+  val[bothinf] <- 0
+  # In the cases where b > a, the result is NaN
+  val[blarger] <- NaN
+
   # Add a to all values
   val <- a + val
+
   return(val)
 }
 
 # Helper function to compute log(exp(a) + exp(b)). Useful for adding
 # probabilities on the log scale
 logsumexp <- function(a, b) {
+  # If either a or b is NaN, that will carry through without issue
+  bothinf <- is.infinite(a) & is.infinite(b) & (a == b)
   negdiff <- -abs(a - b)
-  negdiff[(a == -Inf) & (b == -Inf)] <- 0 # Fix cases where a == b == -Inf
+  negdiff[bothinf] <- 0 # Fix cases where a == b == -Inf or Inf
   pmax(a, b) + log1p(exp(negdiff))
 }
 
