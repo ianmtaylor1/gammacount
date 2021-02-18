b7bc43b6d727fabe0ed212262ec629413541f16a
Ian Taylor <ian@iantaylor.xyz>
2021-02-18 16:02:30 -0700

    More tests of gc distribution.

--- tests/testthat/test-gammacount.R
+++ tests/testthat/test-gammacount.R
@@ -1,4 +1,4 @@
-test_that("gammacount reduces to poisson", {
+test_that("reduces to poisson", {
   x <- seq(0, 200)
   for (lambda in c(0.01, 0.5, 1, 1.5, 10, 20, 100)) {
     expect_equal(dgc(x, lambda, alpha=1), dpois(x, lambda))
@@ -6,7 +6,7 @@ test_that("gammacount reduces to poisson", {
   }
 })
 
-test_that("gammacount cdf is sum of pmf", {
+test_that("cdf is sum of pmf", {
   alpha.vals <- c(0.1, 1, 10)
   lambda.vals <- c(0.5, 5, 50)
   x <- seq(0, 100)
@@ -19,17 +19,64 @@ test_that("gammacount cdf is sum of pmf", {
   }
 })
 
+test_that("cdf and quantile functions are inverses", {
+  # Test p -> q
+  # Uses lower.tail=F and log.p=T to handle large values of x gracefully
+  x <- seq(0, 100)
+  lambda <- recycle(c(0.5, 5, 10, 0.5, 5, 10, 0.5, 5, 10), length(x))
+  alpha <- recycle(c(0.1, 0.1, 0.1, 1, 1, 1, 10, 10, 10), length(x))
+  p <- pgc(x, lambda, alpha, lower.tail=FALSE, log.p=TRUE)
+  expect_equal(qgc(p, lambda, alpha, lower.tail=FALSE, log.p=TRUE), x)
+  # Test q -> p
+  p <- seq(0.01, 0.99, by=.01)
+  lambda <- recycle(c(0.5, 5, 10, 0.5, 5, 10, 0.5, 5, 10), length(p))
+  alpha <- recycle(c(0.1, 0.1, 0.1, 1, 1, 1, 10, 10, 10), length(p))
+  q <- qgc(p, lambda, alpha)
+  expect_true(all(pgc(q, lambda, alpha) >= p))
+  expect_true(all(pgc(q-1, lambda, alpha) < p))
+})
+
 test_that("log equals the log", {
+  # density and distribution functions
   x <- seq(0, 8)
-  lambda <- rep(c(0.5, 5, 10), 3)
-  alpha <- rep(c(0.1, 1, 10), 3)
+  lambda <- recycle(c(0.5, 5, 10, 0.5, 5, 10, 0.5, 5, 10), length(x))
+  alpha <- recycle(c(0.1, 0.1, 0.1, 1, 1, 1, 10, 10, 10), length(x))
   expect_equal(dgc(x, lambda, alpha, log=TRUE), log(dgc(x, lambda, alpha)))
   expect_equal(pgc(x, lambda, alpha, log.p=TRUE), log(pgc(x, lambda, alpha)))
+  # quantile functions
+  p <- seq(0.01, 0.99, by=.01)
+  lambda <- recycle(c(0.5, 5, 10, 0.5, 5, 10, 0.5, 5, 10), length(p))
+  alpha <- recycle(c(0.1, 0.1, 0.1, 1, 1, 1, 10, 10, 10), length(p))
+  expect_equal(qgc(p, lambda, alpha), qgc(log(p), lambda, alpha, log.p=TRUE))
 })
 
 test_that("upper tail equals one minus", {
+  # density and distribution functions
   x <- seq(0, 8)
-  lambda <- rep(c(0.5, 5, 10), 3)
-  alpha <- rep(c(0.1, 1, 10), 3)
+  lambda <- recycle(c(0.5, 5, 10, 0.5, 5, 10, 0.5, 5, 10), length(x))
+  alpha <- recycle(c(0.1, 0.1, 0.1, 1, 1, 1, 10, 10, 10), length(x))
   expect_equal(pgc(x, lambda, alpha, lower.tail=FALSE), 1 - pgc(x, lambda, alpha))
+  # quantile function
+  p <- seq(0.01, 0.99, by=.01)
+  lambda <- recycle(c(0.5, 5, 10, 0.5, 5, 10, 0.5, 5, 10), length(p))
+  alpha <- recycle(c(0.1, 0.1, 0.1, 1, 1, 1, 10, 10, 10), length(p))
+  expect_equal(qgc(p, lambda, alpha), qgc(1 - p, lambda, alpha, lower.tail=FALSE))
+})
+
+test_that("output is correct length", {
+  lengths <- c(3, 6, 12)
+  for (alpha.length in lengths) {
+    alpha <- seq_len(alpha.length)
+    for (lambda.length in lengths) {
+      lambda <- seq_len(lambda.length)
+      for (n in lengths) {
+        x <- seq_len(n)
+        expect_length(rgc(n, lambda, alpha), n)
+        expected.length <- max(n, lambda.length, alpha.length)
+        expect_length(dgc(x, lambda, alpha), expected.length)
+        expect_length(pgc(x, lambda, alpha), expected.length)
+        expect_length(qgc(x/(max(x)+1), lambda, alpha), expected.length)
+      }
+    }
+  }
 })
